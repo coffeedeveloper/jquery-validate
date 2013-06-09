3248a357b83a6ce2d030996e640fa5d14e47e140
coffeedeveloper <coffeedeveloper@gmail.com>
2013-06-09 03:01:34 -0700

    can validate input, but submit no

--- validate.js
+++ validate.js
@@ -1,77 +1,114 @@
-$.fn.validate = function(options){
+$.fn.validate = function (options) {
 	var defaults = {
 		sucClass: 'suc',
 		errClass: 'err',
 		success: $.noop,
 		error: $.noop,
 		autoFocus: false,
-		validateType: 'blur'
+		activeType: 'blur'
 	}
 	var opts = $.extend(defaults, options);
 
 	var $self = $(this),
-		errArr = [];
+		errArr = [],
+		isActive = options == undefined;
 
-	var CheckVal = function(type, val, item){
-		switch(type.toLowerCase()){
+	var CheckVal = function (type, val, $item) {
+		var result = true;
+		switch (type.toLowerCase()) {
 			case 'url':
 				break;
 			case 'email':
 				break;
 			case 'integer':
 				break;
-			case 'range':
-				var r = $(item).data('validateRange').split(',');
-				if (val.length < r[0] || val.length > r[1]) {
-					return false;
+			case 'required':
+				if (!val) {
+					result = false;
 				}
 				break;
-			case 'reguar':
-				var reg = $(item).data('validateReguar');
-				if (!reg.test(val)) {
-					return false;
+			case 'range':
+				var r = $item.data('validateRange').split(',');
+				if (val.length < r[0] || val.length > r[1]) {
+					result = false;
 				}
+				break;
+			case 'regular':
+				var reg = $item.data('validateRegular');
+				if (!reg.test(val)) {
+					result = false;
+				}
+			case 'ajax':
+				window[$item.data('validateAjax')]($item);
 			default:
 				break;
 		}
-		return true;
+		if (!result) {
+			MakeErr($item, type);
+		}
+		return result;
 	}
 
-	var Status = function(item, status, type){
+	var MakeErr = function ($item, type) {
+		errArr.push({
+			item: $item,
+			type: type,
+			msg: $item.data('validate-' + type + '-msg')
+		});
+	}
+
+	var Status = function ($item, status, type) {
 		if (status) {
-			item.addClass(opts.sucClass).removeClass(opts.errClass);
-		}else{
-			$this.addClass(opts.errClass).removeClass(opts.sucClass);
-			errArr.push({
-				item: item,
-				type: type,
-				msg: $this.data('validateMsg')
-			});
+			$item.addClass(opts.sucClass).removeClass(opts.errClass);
+		} else {
+			$item.addClass(opts.errClass).removeClass(opts.sucClass);
 		}
 		return status;
 	}
 
-	var checked = false;
-	$self.find('input[data-validate-required], input[data-validate-type]').each(function(i, v){
+	var Active = function () {
 		var $this = $(this);
-		if ($this.data('validateRequired')) {
-			checked = Status($this, $this.val() ? true : false, 'required');
+		var types = $this.data('validateType').split('|');
+		var checked = true;
+		errArr = isActive ? errArr : [];
+		for (var i = 0; i < types.length && checked; i++) {
+			if (!Status($this, CheckVal(types[i], $this.val(), $this), types)) {
+				checked = false;
+			}
 		}
-		if ($this.data('validateType') && checked) {
-			Status($this, !CheckVal($this.data('validateType'), $this.val(), $this), $this.data('validateType'));
+		if (!isActive) {
+			if (checked) {
+				if (typeof opts.success == 'function') {
+					opts.success([$this]);
+				}
+			} else {
+				if (typeof opts.error == 'function') {
+					opts.error(errArr);
+				}
+			}
 		}
-	});
+	}
 
-	if(errArr.length == 0){
-		if(typeof opts.success == 'function'){
-			opts.success();
+	if (isActive) {
+		var $list = $self.find('input[data-validate-type]');
+		$list.each(Active);
+		if (errArr.length == 0) {
+			if (typeof opts.success == 'function') {
+				opts.success($list);
+			}
+		} else {
+			if (opts.autoFocus) {
+				errArr[0].item.focus();
+			}
+			if (typeof opts.error == 'function') {
+				console.log(errArr);
+				opts.error(errArr);
+			}
 		}
-	}else{
-		if(opts.autoFocus){
-			errArr[0].item.focus();
-		}
-		if(typeof opts.error == 'function'){
-			opts.error(errArr);
-		}
+		return errArr.length == 0;
+	} else {
+		$self.find('input[data-validate-type]').each(function (i, v) {
+			$(this).on(opts.activeType, Active);
+		});
 	}
 }
