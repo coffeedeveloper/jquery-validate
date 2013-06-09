f4beaea75543f4f776b9020d28621c577b9375e4
coffeedeveloper <coffeedeveloper@gmail.com>
2013-06-09 00:36:56 -0700

    version v1.0 make range and reguar version

--- validate.js
+++ validate.js
@@ -1,41 +1,64 @@
 $.fn.validate = function(options){
 	var defaults = {
-		//prefix: 'validate'
 		sucClass: 'suc',
 		errClass: 'err',
 		success: $.noop,
-		error: $.noop
+		error: $.noop,
+		autoFocus: false,
+		validateType: 'blur'
 	}
 	var opts = $.extend(defaults, options);
 
 	var $self = $(this),
 		errArr = [];
 
-	var CheckVal = function(type, val){
+	var CheckVal = function(type, val, item){
 		switch(type.toLowerCase()){
 			case 'url':
-				console.log('url');
 				break;
 			case 'email':
-				console.log('email');
 				break;
 			case 'integer':
-				console.log('integer');
 				break;
+			case 'range':
+				var r = $(item).data('validateRange').split(',');
+				if (val.length < r[0] || val.length > r[1]) {
+					return false;
+				}
+				break;
+			case 'reguar':
+				var reg = $(item).data('validateReguar');
+				if (!reg.test(val)) {
+					return false;
+				}
 			default:
 				break;
 		}
+		return true;
 	}
 
-	//var require = 'data-' + opts.prefix + 'require';
-	$self.find('input[data-validate-require]').each(function(){
-		if(!$(this).val()){
-			errArr.push({
-				item:$(this).addClass(opts.sucClass).removeClass(opts.errClass),
-				msg: $(this).data('validate-msg');
-			});
+	var Status = function(item, status, type){
+		if (status) {
+			item.addClass(opts.sucClass).removeClass(opts.errClass);
 		}else{
-			$(this).addClass(opts.errClass).removeClass(opts.sucClass);
+			$this.addClass(opts.errClass).removeClass(opts.sucClass);
+			errArr.push({
+				item: item,
+				type: type,
+				msg: $this.data('validateMsg')
+			});
+		}
+		return status;
+	}
+
+	var checked = false;
+	$self.find('input[data-validate-required], input[data-validate-type]').each(function(i, v){
+		var $this = $(this);
+		if ($this.data('validateRequired')) {
+			checked = Status($this, $this.val() ? true : false, 'required');
+		}
+		if ($this.data('validateType') && checked) {
+			Status($this, !CheckVal($this.data('validateType'), $this.val(), $this), $this.data('validateType'));
 		}
 	});
 
@@ -44,7 +67,10 @@ $.fn.validate = function(options){
 			opts.success();
 		}
 	}else{
-		if(typeof opts.error == function(){
+		if(opts.autoFocus){
+			errArr[0].item.focus();
+		}
+		if(typeof opts.error == 'function'){
 			opts.error(errArr);
 		}
 	}
